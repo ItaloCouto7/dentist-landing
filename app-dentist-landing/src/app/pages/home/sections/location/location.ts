e7863b73285de86949aef8a58b204603cf7a80ac
Italo Coutinho <coutinhoitalo@gmail.com>
2026-01-08 15:03:45 -0300

    feat: Estilizacao e modificacao da section de contact

--- app-dentist-landing/src/app/pages/home/sections/location/location.ts
+++ app-dentist-landing/src/app/pages/home/sections/location/location.ts
@@ -13,33 +13,33 @@ interface ContactInfo {
   standalone: true,
   imports: [CommonModule],
   templateUrl: './location.html',
-  styleUrls: ['./location.scss']
+  styleUrls: ['./location.scss'],
 })
 export class LocationComponent {
   contactInfo: ContactInfo[] = [
     {
       icon: 'phone',
       title: 'Telefone / WhatsApp',
-      info: '(81) 99999-9999',
-      subinfo: 'Segunda a Sexta: 8h às 18h'
+      info: '(81) 99597-0362',
+      subinfo: 'Segunda a Sexta: 8h às 18h',
     },
     {
       icon: 'clock',
       title: 'Horário de Atendimento',
       info: 'Segunda a Sexta: 8h às 18h',
-      subinfo: 'Sábado: 9h às 13h'
+      subinfo: 'Sábado: 8h às 13h',
     },
     {
       icon: 'mail',
       title: 'E-mail',
-      info: 'contato@carolinichaves.com.br',
-      subinfo: 'Respondemos em até 24h'
+      info: 'carolinicchaves@hotmail.com',
+      subinfo: 'Respondemos em até 24h',
     },
     {
       icon: 'map',
       title: 'Atendimento',
-      info: 'Atendimento domiciliar em Recife',
-      subinfo: 'Consulte disponibilidade na sua região'
-    }
+      info: 'Atendimento em consultório agendado',
+      subinfo: 'Local definido em conjunto com o paciente',
+    },
   ];
 }
