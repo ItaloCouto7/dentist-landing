f7db7f8f8880347f97491dbcd00fd4c4db914946
Italo Coutinho <coutinhoitalo@gmail.com>
2026-01-09 16:03:20 -0300

    feat: Finalizacao da landing page

--- app-dentist-landing/src/app/app.ts
+++ app-dentist-landing/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./pages/home/home";
 
@@ -8,6 +8,24 @@ import { Home } from "./pages/home/home";
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements AfterViewInit{
   protected readonly title = signal('app-dentist-landing');
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('reveal');
+            observer.unobserve(entry.target); // anima só uma vez
+          }
+        });
+      },
+      { threshold: 0.15 }
+    );
+
+    document.querySelectorAll('.reveal-item').forEach(el => {
+      observer.observe(el);
+    });
+  }
 }
